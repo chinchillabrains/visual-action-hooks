b6599af8f20cc08fc62213e3efefbeef812eda72
Ilias P <58887367+chinchillabrains@users.noreply.github.com>
2022-02-27 17:24:02 +0200

    Bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,10 @@ jQuery(document).ready(function($) {
         $(document.body).toggleClass('chvh-visible');
     });
     $('.chilla-visual-hook').click(function(e){
-        e.stopPropagation();
-        $(this).addClass('show-name');
-        $(this).find('> .chilla-visual-hook-name').addClass('visible');
+        if ( $(document.body).hasClass('chvh-visible') ) {
+            e.stopPropagation();
+            $(this).addClass('show-name');
+            $(this).find('> .chilla-visual-hook-name').addClass('visible');
+        }
     });
 });
